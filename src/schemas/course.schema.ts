5cf7633606107f67aceb376654bc841e60bd7e9e
Naman Sharma <naman.sharma@noak.sg>
2025-03-25 19:54:00 +0530

    Refactor :- Continue later

---
+++ src/schemas/course.schema.ts
@@ -0,0 +1,18 @@
+// /**
+//  *
+//  * Reference - https://docs.nestjs.com/techniques/mongodb
+//  *
+//  */
+
+// import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+// import { ApiProperty } from '@nestjs/swagger';
+// import mongoose, { HydratedDocument, Types } from 'mongoose';
+
+// export type CourseDocuments = HydratedDocument<Course>;
+
+// @Schema()
+// export class Course {
+//   courseName: string;
+// }
+
+// export const TestimonialSchema = SchemaFactory.createForClass(Testimonial);
