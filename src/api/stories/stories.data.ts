486d584ca28920751ed09259205aa276aad31e22
prakashuniyal01 <uniyalprakash7@gmail.com>
2025-04-09 11:12:01 +0530

    stories update fixed

--- src/api/stories/stories.data.ts
+++ src/api/stories/stories.data.ts
@@ -1,3 +1,35 @@
+// import { Injectable } from '@nestjs/common';
+// import { InjectModel } from '@nestjs/mongoose';
+// import { Model } from 'mongoose';
+// import { Story, StoryDocument } from '../../schemas/stories.schema';
+// import { CreateStoryDto } from '../../dto/stories.dto';
+
+// @Injectable()
+// export class StoriesDataService {
+//   constructor(@InjectModel(Story.name) private storyModel: Model<StoryDocument>) {}
+
+//   async getStories(): Promise<StoryDocument[]> {
+//     return this.storyModel.find().exec();
+//   }
+
+//   async createStory(createStoryDto: CreateStoryDto & { userImageUrl: string; companyLogoUrl: string }): Promise<StoryDocument> {
+//     const newStory = new this.storyModel(createStoryDto);
+//     return newStory.save();
+//   }
+
+//   async getStoryById(id: string): Promise<StoryDocument | null> {
+//     return this.storyModel.findById(id).exec();
+//   }
+
+//   async updateStory(id: string, updateStoryDto: Partial<CreateStoryDto & { userImageUrl: string; companyLogoUrl: string }>): Promise<StoryDocument | null> {
+//     return this.storyModel.findByIdAndUpdate(id, updateStoryDto, { new: true }).exec();
+//   }
+
+//   async deleteStory(id: string): Promise<StoryDocument | null> {
+//     return this.storyModel.findByIdAndDelete(id).exec();
+//   }
+// }
+
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
@@ -5,7 +37,7 @@ import { Story, StoryDocument } from '../../schemas/stories.schema';
 import { CreateStoryDto } from '../../dto/stories.dto';
 
 @Injectable()
-export class StoriesDataService {
+export class StoryDataService {
   constructor(@InjectModel(Story.name) private storyModel: Model<StoryDocument>) {}
 
   async getStories(): Promise<StoryDocument[]> {
