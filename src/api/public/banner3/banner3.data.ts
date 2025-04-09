4ab73569b4dba1e1f8c7df7947d9eb15ae6d754a
prakashuniyal01 <uniyalprakash7@gmail.com>
2025-04-09 10:46:45 +0530

    fixed put request

--- src/api/public/banner3/banner3.data.ts
+++ src/api/public/banner3/banner3.data.ts
@@ -1,3 +1,36 @@
+// import { Injectable } from '@nestjs/common';
+// import { InjectModel } from '@nestjs/mongoose';
+// import { Model } from 'mongoose';
+// import { Banner3, Banner3Document } from '../../../schemas/home/banner3.schema';
+// import { CreateBanner3Dto } from '../../../dto/home/banner3.dto';
+
+// @Injectable()
+// export class Banner3DataService {
+//   constructor(@InjectModel(Banner3.name) private banner3Model: Model<Banner3Document>) {}
+
+//   async getBanner3s(): Promise<Banner3Document[]> {
+//     return this.banner3Model.find().exec();
+//   }
+
+//   async createBanner3(createBanner3Dto: CreateBanner3Dto & { imageUrl: string }): Promise<Banner3Document> {
+//     const newBanner3 = new this.banner3Model(createBanner3Dto);
+//     return newBanner3.save();
+//   }
+
+//   async getBanner3ById(id: string): Promise<Banner3Document | null> {
+//     return this.banner3Model.findById(id).exec();
+//   }
+
+//   async updateBanner3(id: string, updateBanner3Dto: Partial<CreateBanner3Dto & { imageUrl: string }>): Promise<Banner3Document | null> {
+//     return this.banner3Model.findByIdAndUpdate(id, updateBanner3Dto, { new: true }).exec();
+//   }
+
+//   async deleteBanner3(id: string): Promise<Banner3Document | null> {
+//     return this.banner3Model.findByIdAndDelete(id).exec();
+//   }
+// }
+
+
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
--- src/api/public/stats/stats.data.ts
+++ src/api/public/stats/stats.data.ts
@@ -1,3 +1,43 @@
+// import { Injectable } from '@nestjs/common';
+// import { InjectModel } from '@nestjs/mongoose';
+// import { Model } from 'mongoose';
+// import { Stats, StatsDocument } from '../../../schemas/home/stats.schema';
+// import { CreateStatsDto } from '../../../dto/home/stats.dto';
+
+// @Injectable()
+// export class StatsDataService {
+//   constructor(
+//     @InjectModel(Stats.name) private statsModel: Model<StatsDocument>,
+//   ) {}
+
+//   async getStats(): Promise<StatsDocument[]> {
+//     return this.statsModel.find().exec();
+//   }
+
+//   async createStats(createStatsDto: CreateStatsDto & { imageUrl: string }): Promise<StatsDocument> {
+//     const newStats = new this.statsModel(createStatsDto);
+//     return newStats.save();
+//   }
+
+//   async getStatsById(id: string): Promise<StatsDocument | null> {
+//     return this.statsModel.findById(id).exec();
+//   }
+
+//   async updateStats(
+//     id: string,
+//     updateStatsDto: Partial<CreateStatsDto & { imageUrl: string }>,
+//   ): Promise<StatsDocument | null> {
+//     return this.statsModel
+//       .findByIdAndUpdate(id, updateStatsDto, { new: true })
+//       .exec();
+//   }
+
+//   async deleteStats(id: string): Promise<StatsDocument | null> {
+//     return this.statsModel.findByIdAndDelete(id).exec();
+//   }
+// }
+
+
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
@@ -6,9 +46,7 @@ import { CreateStatsDto } from '../../../dto/home/stats.dto';
 
 @Injectable()
 export class StatsDataService {
-  constructor(
-    @InjectModel(Stats.name) private statsModel: Model<StatsDocument>,
-  ) {}
+  constructor(@InjectModel(Stats.name) private statsModel: Model<StatsDocument>) {}
 
   async getStats(): Promise<StatsDocument[]> {
     return this.statsModel.find().exec();
@@ -23,13 +61,8 @@ export class StatsDataService {
     return this.statsModel.findById(id).exec();
   }
 
-  async updateStats(
-    id: string,
-    updateStatsDto: Partial<CreateStatsDto & { imageUrl: string }>,
-  ): Promise<StatsDocument | null> {
-    return this.statsModel
-      .findByIdAndUpdate(id, updateStatsDto, { new: true })
-      .exec();
+  async updateStats(id: string, updateStatsDto: Partial<CreateStatsDto & { imageUrl: string }>): Promise<StatsDocument | null> {
+    return this.statsModel.findByIdAndUpdate(id, updateStatsDto, { new: true }).exec();
   }
 
   async deleteStats(id: string): Promise<StatsDocument | null> {
