d2de1efde2690cb44725544b75ad73e3466d9989
prakashuniyal01 <uniyalprakash7@gmail.com>
2025-04-15 18:19:34 +0530

    fixed contect us api

--- src/dto/home/contact-us.dto.ts
+++ src/dto/home/contact-us.dto.ts
@@ -1,3 +1,73 @@
+// import { ApiProperty } from '@nestjs/swagger';
+// import { IsString, IsNotEmpty, IsOptional } from 'class-validator';
+
+// export class CreateContactUsDto {
+//   @ApiProperty()
+//   @IsString()
+//   @IsNotEmpty()
+//   name: string;
+
+//   @ApiProperty()
+//   @IsString()
+//   @IsNotEmpty()
+//   email: string;
+
+//   @ApiProperty({ required: false })
+//   @IsString()
+//   @IsOptional()
+//   number?: string; // Optional field
+
+//   @ApiProperty()
+//   @IsString()
+//   @IsNotEmpty()
+//   subject: string;
+
+//   @ApiProperty()
+//   @IsString()
+//   @IsOptional()
+//   sourse: string;
+
+//   @ApiProperty()
+//   @IsString()
+//   @IsNotEmpty()
+//   message: string;
+// }
+
+// export class ContactUs {
+//   @ApiProperty()
+//   _id: string;
+
+//   @ApiProperty()
+//   name: string;
+
+//   @ApiProperty()
+//   email: string;
+
+//   @ApiProperty({ required: false })
+//   number?: string; // Optional field
+
+//   @ApiProperty()
+//   subject: string;
+
+//   @ApiProperty()
+//   sourse: string;
+
+//   @ApiProperty()
+//   message: string;
+
+//   @ApiProperty()
+//   createdAt: Date;
+
+//   @ApiProperty()
+//   updatedAt: Date;
+// }
+
+// export class GetContactUsResponseDTO {
+//   @ApiProperty({ type: [ContactUs] })
+//   contactUs: ContactUs[];
+// }
+
+
 import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsNotEmpty, IsOptional } from 'class-validator';
 
@@ -15,17 +85,17 @@ export class CreateContactUsDto {
   @ApiProperty({ required: false })
   @IsString()
   @IsOptional()
-  number?: string; // Optional field
+  number?: string;
 
   @ApiProperty()
   @IsString()
   @IsNotEmpty()
   subject: string;
 
-  @ApiProperty()
+  @ApiProperty({ required: false })
   @IsString()
   @IsOptional()
-  sourse: string;
+  sourse?: string;
 
   @ApiProperty()
   @IsString()
@@ -44,13 +114,13 @@ export class ContactUs {
   email: string;
 
   @ApiProperty({ required: false })
-  number?: string; // Optional field
+  number?: string;
 
   @ApiProperty()
   subject: string;
 
-  @ApiProperty()
-  sourse: string;
+  @ApiProperty({ required: false })
+  sourse?: string;
 
   @ApiProperty()
   message: string;
