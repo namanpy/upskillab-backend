4b33751dbce69c2f2514d5fdcfbfe61c57086547
prakashuniyal01 <uniyalprakash7@gmail.com>
2025-05-03 16:25:17 +0530

    fixed

--- src/api/enrollment/enrollment.module.ts
+++ src/api/enrollment/enrollment.module.ts
@@ -37,5 +37,6 @@ import { EnrollmentDataService } from './enrollment.data';
   ],
   controllers: [EnrollmentController],
   providers: [EnrollmentLogicService, EnrollmentDataService],
+  exports: [EnrollmentDataService],
 })
 export class EnrollmentModule {}
